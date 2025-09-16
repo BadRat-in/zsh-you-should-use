306492e7d485d6ced63002c222659ba19fbbc3a1
BadRat-in <rbmorena42@gmail.com>
2025-09-16 21:18:15 +0530

    feat: ✨ Added  to make it compatible with `zsc`

--- zsh-you-should-use.zsh
+++ zsh-you-should-use.zsh
@@ -6,7 +6,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-export YSU_VERSION='1.8.0'
+export YSU_VERSION='2.0.0'
 
 if ! type "tput" > /dev/null; then
     printf "WARNING: tput command not found on your PATH.\n"
